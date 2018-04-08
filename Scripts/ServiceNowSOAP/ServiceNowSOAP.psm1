9b8beb94ffe3db387e55e8a89edd254818160ece
harbingereternal <dstabler84@gmail.com>
2018-04-08 13:20:38 -0400

    Conversion to module
    * Moved scripts into a module format.
    * Changed the name of Create-ServiceNowTicketSOAP to New-ServiceNowTicketSOAP due to unapproved verb warning.

---
+++ Scripts/ServiceNowSOAP/ServiceNowSOAP.psm1
@@ -0,0 +1,23 @@
+﻿
+
+#Requires -Version 3.0
+[cmdletbinding()]
+param()
+
+Write-Verbose $PSScriptRoot
+
+Write-Verbose 'Import everything in subfolders'
+foreach ($Folder in @('Private','Public')) {
+
+$Root = Join-Path -Path $PSScriptRoot -ChildPath $Folder
+    if (Test-Path -Path $Root) {
+        Write-Verbose "Processing folder $Root"
+        $Files = Get-ChildItem -Path $Root -Filter *.ps1 -Recurse
+
+        $Files | Where-Object{ $_.Name -notlike '*.Tests.ps1' } |
+            ForEach-Object { Write-Verbose $_.BaseName; . $PSItem.FullName }
+    }
+
+}
+
+Export-ModuleMember -Function (Get-ChildItem -Path "$PSScriptRoot\Public\*.ps1").BaseName
